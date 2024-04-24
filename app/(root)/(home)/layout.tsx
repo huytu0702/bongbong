cb0d4a87ad2d52d8f8a9ebb1d4b4de513880e1d5
unknown <mailcualongk@gmail.com>
2024-04-24 16:58:26 +0700

    first commit

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,15 @@
 import NavBar from '@/components/NavBar'
 import SideBar from '@/components/SideBar'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "YOOM",
+  description: "Video calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
 
 const Homelayout = ({ children }: { children: ReactNode}) => {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+import 'react-datepicker/dist/react-datepicker.css'
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
